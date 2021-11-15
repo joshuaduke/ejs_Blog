b0fc252f50839f738cd5922c267939338aa3b56b
Joshua Duke <dukejoshuae@gmail.com>
2021-11-15 15:14:24 -0500

    complete challenge 2 && 3

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', (req, res)=>{
-  res.render('home')
+  res.render('home', {homeContent: homeStartingContent})
 })
 
 
